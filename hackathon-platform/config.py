21ecdec10d38c9a1c161d5153ec73d4050cb9c02
Nithish Reddy <nithishreddyb2@gmail.com>
2025-08-18 17:54:14 +0530

    updated config

--- hackathon-platform/config.py
+++ hackathon-platform/config.py
@@ -2,7 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "dev_secret_key"
-    SQLALCHEMY_DATABASE_URI = (
-        "mysql+pymysql://root:test1234@localhost/hackathon_db"
-    )
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:test1234@localhost/hackdb"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = "supersecretjwtkey"  
