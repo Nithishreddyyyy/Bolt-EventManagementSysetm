a5f236705a6dae6de73d50dbae964b1875a67186
Nithish Reddy <nithishreddyb2@gmail.com>
2025-08-18 15:34:55 +0530

    models added

---
+++ hackathon-platform/models/event.py
@@ -0,0 +1,9 @@
+from models import db
+
+class Event(db.Model):
+    __tablename__ = "events"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    theme = db.Column(db.String(120))
+    rules = db.Column(db.Text)
